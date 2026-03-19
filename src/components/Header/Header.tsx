f7d2567ccd62e12722c4fb6e5fbad9de6e8e2d2c
NigarHasanzda <nigarhesenzade703@gmail.com>
2026-03-19 22:49:46 +0400

    Sidebar design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
 
   return (
     <>
-      <header className="w-full py-6 flex justify-center sticky top-0 bg-white z-50">
+      <header className="w-full py-6 flex justify-center sticky top-0  z-50">
         <div className="w-full max-w-[90%] bg-white rounded-[20px] shadow-[0_4px_30px_rgba(0,0,0,0.06)] px-6 py-[18px] flex items-center justify-between border border-gray-50/50">
 
           <Link href="/" className="flex items-center gap-1">
@@ -67,16 +67,14 @@ const Header = () => {
       </header>
 
       <div
-        className={`fixed inset-0 z-50 bg-black bg-opacity-40 transition-opacity duration-300 ${
+        className={`fixed inset-0 z-50 transition-opacity duration-300 ${
           sidebarOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
         }`}
         onClick={toggleSidebar}
       />
 
       <div
-        className={`fixed top-0 right-0 h-full w-[250px] bg-white z-50 shadow-lg transition-transform duration-300 flex flex-col p-6 ${
-          sidebarOpen ? "translate-x-0" : "translate-x-full"
-        }`}
+        className={`fixed top-0 right-0 h-[90%] mt-6 ml-5 w-[230px] rounded-2xl bg-white  z-50 shadow-lg transition-transform duration-300 flex flex-col p-6 ${   sidebarOpen ? "translate-x-0" : "translate-x-full" }`}
       >
         <div className="flex justify-end mb-6">
           <CloseIcon
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -27,18 +27,11 @@ const Sidebar = () => {
       </div>
 
       {/* Overlay */}
-      <div
-        className={`fixed inset-0 bg-black bg-opacity-40 z-40 transition-opacity duration-300 ${
-          isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
-        }`}
+      <div  className={`fixed inset-0  z-40 transition-opacity duration-300 ${   isOpen ? " pointer-events-auto" : "opacity-0 pointer-events-none" }`}
         onClick={toggleSidebar}
       />
 
-      {/* Sidebar */}
-      <div
-        className={`fixed top-0 right-0 h-full w-[250px] bg-white z-50 shadow-lg transition-transform duration-300 ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } flex flex-col p-6`}
+      <div className={`fixed top-0 right-0 h-full w-[250px] bg-white z-50 shadow-lg transition-transform duration-300 ${  isOpen ? "translate-x-0" : "translate-x-full" } flex flex-col p-6`}
       >
         {/* Close button */}
         <div className="flex justify-end mb-6">
